da6328983b7c4dfa11e77e00348b805332c3e682
MadhuriGhattamaneni <yogesh.luckshetty@gmail.com>
2023-05-07 13:15:29 -0700

    ad

---
+++ frontend/src/Components/Dashboard.js
@@ -0,0 +1,37 @@
+import React, { useState, useContext, useEffect } from 'react';
+import axios from 'axios';
+import { AuthContext } from '../context/AuthProvider';
+import { useNavigate } from "react-router-dom";
+
+
+function Dashboard() {
+    const { guserID, setguserID } = useContext(AuthContext);
+    const { guserRole, setguserRole } = useContext(AuthContext);
+    const { guserEmail, setguserEmail } = useContext(AuthContext);
+    const { guserName, setguserName } = useContext(AuthContext);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if(guserRole == ''){
+            navigate('/');
+        }
+        else if(guserRole == 'member'){
+            navigate('/member');
+        }
+        else if(guserRole == 'Non Member'){
+            navigate('/nonmember');
+        }
+    }, [guserRole]);
+    
+    return (
+        <div>
+            <div>{guserID}</div>
+            <div>{guserRole}</div>
+            <div>{guserEmail}</div>
+            <div>{guserName}</div>
+        </div>
+
+    )
+}
+
+export default Dashboard;
